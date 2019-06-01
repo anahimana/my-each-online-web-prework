00ae833632804524486300558efa70d8bcba5e37
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 17:38:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(arr)
   # code here
+  if as_object?
+    i = 0
+    while i < arr.length
+      yield (arr[i])
+      i += 1
+    end
+  else
+    puts "Sup"
+  end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
