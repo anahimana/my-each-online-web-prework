a12b9dc48a40080ae6d48225621c03f7aa940b5e
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 17:38:30 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,6 @@
 def my_each(arr)
   # code here
-  if as_object?
+  if block_given?
     i = 0
     while i < arr.length
       yield (arr[i])
@@ -14,4 +14,20 @@ end
 collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
+end
+
+
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
